d366ffbec15e4d89d4460b7e0db69e598f222723
Shintaro Shiba <shiba.shintaro@gmail.com>
2016-08-09 08:48:57 +0900

    fixed bug

--- train.py
+++ train.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # model setteing
-    model = HumanPartsNet(n_class=24)
+    model = HumanPartsNet(n_class=25)
     if args.pretrainedmodel is not None:
         from chainer import serializers
         serializers.load_npz(args.pretrainedmodel, model)
