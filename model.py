35a5350d6b29cd0edd2a72251657a135ba4c25c4
bobye <yelpoo@gmail.com>
2017-01-03 10:59:02 -0500

    use internal accuracy; calculate_accuray() still has bugs

--- model.py
+++ model.py
@@ -83,7 +83,8 @@ class HumanPartsNet(chainer.Chain):
     def __call__(self, x, t):
         h = self.predict_proba(x)
         self.loss = F.softmax_cross_entropy(h, t)
-        self.accuracy = self.calculate_accuracy(h, t)
+        #self.accuracy = self.calculate_accuracy(h, t)
+        self.accuracy = F.accuracy(h, t, ignore_label = -1)
         return self.loss
 
     def predict(self, x):
