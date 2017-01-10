880f0575ae96683bf6c58fa8b18487ba9fe2f921
Marc Kassubeck <kassubeck@cg.cs.tu-bs.de>
2017-01-10 12:01:38 +0100

    Make visualization compatible with training

--- visualize.py
+++ visualize.py
@@ -8,6 +8,7 @@ import time
 from model import HumanPartsNet
 from data import MiniBatchLoader
 import chainer
+from os.path import basename
 
 import cv2
 
@@ -15,6 +16,9 @@ resultdir = "./result/"
 X_dir = "./data/img/"
 y_dir = "./data/mask/"
 
+def standardize(image):
+    subtracted_img = images - 126
+    return subtracted_img / 255.
 
 parser = argparse.ArgumentParser(description='Human parts network')
 parser.add_argument('--gpu', '-g', default=-1, type=int,
@@ -26,18 +30,16 @@ parser.add_argument('--file', '-f', type=str,
 args = parser.parse_args()
 
 # model setteing
-model = HumanPartsNet(n_class=25)
+model = HumanPartsNet(n_class=15)
 if args.pretrainedmodel is not None:
     from chainer import serializers
     serializers.load_npz(args.pretrainedmodel, model)
 
-img = np.transpose(np.expand_dims(cv2.imread(args.file), 0), (0, 3, 1, 2)) / 255.
+# img = np.transpose(np.expand_dims(cv2.imread(args.file), 0), (0, 3, 1, 2)) / 255.
+bname = basename(args.file)
+img = np.transpose(np.expand_dims(standardize(cv2.resize(cv2.imread(args.file), (300, 300)).astype(np.uint8)), 0), (0, 3, 1, 2))
 x = chainer.Variable(img.astype(np.float32), volatile='on')
 y = model.predict(x)
 mask = np.argmax(y.data[0], axis=0)
 
-print mask
-"""
-now writing next...
-"""
-
+np.save(resultdir + bname + '.npy', mask)
